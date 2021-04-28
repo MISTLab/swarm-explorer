580fe0ed4c8b81e5bc1054c6e8c683fa93eb7663
1123 <1123@example.com>
2021-04-28 18:19:53 -0400

    Fix object detection Dockerfile

--- object-detection/entrypoint.sh
+++ object-detection/entrypoint.sh
@@ -3,6 +3,6 @@
 export ROS_MASTER_URI=http://192.168.143.125:11311;
 export ROS_IP=192.168.143.125;
 
-echo "source /sensing-ws/devel/setup.bash" > ~/.bashrc
+source /sensing-ws/install/setup.bash
 
-/bin/bash
+rosrun object_detection main
