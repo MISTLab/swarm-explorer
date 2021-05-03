3fdb0d34bac35bfe1c34c2e8ed7c270f904552f5
1123 <1123@example.com>
2021-05-03 17:40:14 -0400

    Fix entrypoint

--- robot/entrypoint.sh
+++ robot/entrypoint.sh
@@ -5,6 +5,23 @@ export ROS_IP=192.168.143.125;
 
 echo "source /gbplanner_ws/devel/setup.bash" > ~/.bashrc
 source /gbplanner_ws/devel/setup.bash
-roslaunch gbplanner rover_outdoor.launch
 
-#/bin/bash
+case "$1" in
+        explore)
+            roslaunch gbplanner rover_outdoor.launch
+            ;;
+
+        bridge)
+            source /opt/ros/dashing/setup.bash
+            export ROS_DOMAIN_ID=$2
+            ros2 run ros1_bridge dynamic_bridge --bridge-all-1to2-topics
+            ;; 
+
+        bash)
+            /bin/bash
+            ;;
+        *)
+            echo $"Usage: $1 {explore|bridge|bash}"
+            exit 1
+ 
+esac
