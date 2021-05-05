995973aa95fd6da306c88c2d0db31c07270d1dbf
Pierre-Yves Lajoie <lajoie.py@gmail.com>
2021-05-05 16:43:42 -0400

    Fix aliases and entrypoint

--- robot/entrypoint.sh
+++ robot/entrypoint.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
-
-export ROS_MASTER_URI=http://192.168.143.125:11311;
-export ROS_IP=192.168.143.125;
-
 echo "source /gbplanner_ws/devel/setup.bash" > ~/.bashrc
 source /gbplanner_ws/devel/setup.bash
 
-case "$1" in
+export ROS_MASTER_URI=http://$1:11311;
+export ROS_IP=$1;
+
+case "$2" in
         explore)
-            roslaunch gbplanner rover_outdoor.launch robot_id:=$2
+            roslaunch gbplanner rover_outdoor.launch robot_id:=$3
             ;;
 
         bridge)
             source /opt/ros/dashing/setup.bash
-            export ROS_DOMAIN_ID=$2
             ros2 run ros1_bridge dynamic_bridge --bridge-all-1to2-topics
             ;; 
 
@@ -26,7 +24,7 @@ case "$1" in
             /bin/bash
             ;;
         *)
-            echo $"Usage: $1 {explore|bridge|bash}"
+            echo $"Usage: $2 {explore|bridge|bash}"
             exit 1
  
 esac
